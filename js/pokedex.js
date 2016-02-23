326d4f12ce845a7cc53d8ccf3224a566a43f3cad
gigouni <nicolas.gigou@gmail.com>
2016-02-23 12:49:19 +0100

    Pokemon selected in the list and displayed : OK.

--- js/pokedex.js
+++ js/pokedex.js
@@ -5,14 +5,19 @@ pokeApp.config(['$resourceProvider', function($resourceProvider) {
     $resourceProvider.defaults.stripTrailingSlashes = false;
 }]);
 
+// The URL to access to the API providing the Pokemon list
 var pokeApiUrl = "http://pokeapi.co/";
 
-// Controller for search usage
+
+
+// -------------------------------------------------
+// Controllers
+// -------------------------------------------------
 function SearchController($scope, $http)
 {
     console.log("LOG : CONTROLLER : Controller SearchController OK");
 
-    var pokeApiUrlListTotal = "http://pokeapi.co/api/v2/pokemon/";
+    var pokeApiUrlListTotal = pokeApiUrl + "api/v2/pokemon/";
     console.log("LOG : CONTROLLER : URL fournissant la liste des pokémons : " + pokeApiUrlListTotal);
 
     // GET request
@@ -21,6 +26,11 @@ function SearchController($scope, $http)
         url: pokeApiUrlListTotal
     }).then(function successCallback(response) {
         console.log("LOG : CONTROLLER : Requête HTTP réussit avec succès");
-        $scope.pokemons = response.data;
+        $scope.data = {
+            // To display the name of the selected pokemon
+            selectedPoke: null,
+            // To provide the list of pokemons
+            pokemons: response.data
+        };
     });
 }
