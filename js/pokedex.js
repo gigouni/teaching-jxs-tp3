a2c6366b89b563ea774fb45a8c676c2b01915335
gigouni <nicolas.gigou@gmail.com>
2016-02-23 12:02:08 +0100

    HTTP GET request ok. List of pokemons displayed successfully.

--- js/pokedex.js
+++ js/pokedex.js
@@ -1,7 +1,26 @@
-var pokeApp = angular.module('pokedex', ['ngResource']);
+var pokeApp = angular.module('pokedex', ['ngResource'])
+    .controller('SearchCtrl', SearchController);
 
 pokeApp.config(['$resourceProvider', function($resourceProvider) {
     $resourceProvider.defaults.stripTrailingSlashes = false;
 }]);
 
-var pokeApiUrl = "http://pokeapi.co/"
+var pokeApiUrl = "http://pokeapi.co/";
+
+// Controller for search usage
+function SearchController($scope, $http)
+{
+    console.log("LOG : CONTROLLER : Controller SearchController OK");
+
+    var pokeApiUrlListTotal = "http://pokeapi.co/api/v2/pokemon/";
+    console.log("LOG : CONTROLLER : URL fournissant la liste des pokémons : " + pokeApiUrlListTotal);
+
+    // GET request
+    $http({
+        method: 'GET',
+        url: pokeApiUrlListTotal
+    }).then(function successCallback(response) {
+        console.log("LOG : CONTROLLER : Requête HTTP réussit avec succès");
+        $scope.pokemons = response.data;
+    });
+}
